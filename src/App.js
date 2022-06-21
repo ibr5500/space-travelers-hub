b29bd47c27097f2b96e554f71d5751d5d76b812d
GASOMINALI Olivier <ogasominali@gmail.com>
2022-06-21 16:02:05 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,13 +7,13 @@ import Myprofile from './components/Myprofile';
 function App() {
   return (
     <BrowserRouter>
-    <Navbar />
-    <Routes>
-      <Route path="/" element={<div>Rockets</div>} />
-      <Route path="/missions" element={<div>Missions</div>} />
-      <Route path="/myprofile" element={<Myprofile />} />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<div>Rockets</div>} />
+        <Route path="/missions" element={<div>Missions</div>} />
+        <Route path="/myprofile" element={<Myprofile />} />
       </Routes>
-        </BrowserRouter>
+     </BrowserRouter>
   );
 }
 export default App;
