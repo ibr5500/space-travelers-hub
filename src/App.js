f128089f5c32c2f9deeafe97e0df8f0581964f2c
GASOMINALI Olivier <ogasominali@gmail.com>
2022-06-21 15:48:06 +0200

    Fix linters error

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import logo from './assets/image/logo.png';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import logo from './assets/image/logo.png';
 import './App.css';
 import Navbar from './components/Navbar';
 import Myprofile from './components/Myprofile';
 
 function App() {
   return (
-      <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<div>Rockets</div>} />
-        <Route path="/missions" element={<div>Missions</div>} />
-        <Route path="/myprofile" element={<Myprofile />} />
+    <BrowserRouter>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<div>Rockets</div>} />
+      <Route path="/missions" element={<div>Missions</div>} />
+      <Route path="/myprofile" element={<Myprofile />} />
       </Routes>
-    </BrowserRouter>
+        </BrowserRouter>
   );
 }
 export default App;
