57be63d39dfef4eab9881509e8a39abafe873c11
unknown <ibr.s.ahmat@gmail.com>
2022-06-21 12:18:14 +0300

    Add nav tag

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../assets/image/logo.png";
+
+const Navbar = () => (
+  <nav>
+    <h2>{logo}</h2>
+  </nav>
+);
+
+export default Navbar;
