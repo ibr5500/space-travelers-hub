40d04fa1e88eb5f38eaed925e6acf55f53fa7e3a
GASOMINALI Olivier <ogasominali@gmail.com>
2022-06-22 09:06:35 +0200

    Rockets added

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -2,11 +2,12 @@ import { configureStore } from '@reduxjs/toolkit';
 import { combineReducers, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
+import rocketsreducer from './rockets/rockets'
 import missionsReducer from './missions/missions';
 
 const reducer = combineReducers({
   missions: missionsReducer,
-
+  rockets:rocketsReducer
 });
 
 const store = configureStore(reducer, applyMiddleware(thunk, logger));
